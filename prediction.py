314d56753557175e49d9a4db97560dfd6d4b162e
Chris Chase <cchase@redhat.com>
2021-04-19 03:44:21 +0000

    Added object detection model and notebooks.

--- prediction.py
+++ prediction.py
@@ -1,17 +1,49 @@
-# For sample predict functions for popular libraries visit https://github.com/opendatahub-io/odh-prediction-samples
+import tensorflow as tf
+import base64
 
-# Import libraries
-# import tensorflow as tf
-
-# Load your model.
-# model_dir = 'models/myfancymodel'
-# saved_model = tf.saved_model.load(model_dir)
-# predictor = saved_model.signatures['default']
+model_dir = 'models/openimages_v4_ssd_mobilenet_v2_1'
+saved_model = tf.saved_model.load(model_dir)
+detector = saved_model.signatures['default']
 
 
-# Write a predict function 
-def predict(args_dict):
-#     arg = args_dict.get('arg1')
-#     predictor(arg)
-    return {'prediction': 'not implemented'}
+def predict(body):
+    base64img = body.get('image')
+    img_bytes = base64.decodebytes(base64img.encode())
+    detections = detect(img_bytes)
+    cleaned = clean_detections(detections)
+    
+    return { 'detections': cleaned }
 
+
+def detect(img):    
+    image = tf.image.decode_jpeg(img, channels=3)
+    converted_img  = tf.image.convert_image_dtype(image, tf.float32)[tf.newaxis, ...]
+    result = detector(converted_img)
+    num_detections = len(result["detection_scores"])
+    
+    output_dict = {key:value.numpy().tolist() for key, value in result.items()}
+    output_dict['num_detections'] = num_detections
+    
+    return output_dict
+
+
+def clean_detections(detections):
+    cleaned = []
+    max_boxes = 10
+    num_detections = min(detections['num_detections'], max_boxes)
+
+    for i in range(0, num_detections):
+        d = {
+            'box': {
+                'yMin': detections['detection_boxes'][i][0],
+                'xMin': detections['detection_boxes'][i][1],
+                'yMax': detections['detection_boxes'][i][2],
+                'xMax': detections['detection_boxes'][i][3]
+            },
+            'class': detections['detection_class_entities'][i].decode('utf-8'),
+            'label': detections['detection_class_entities'][i].decode('utf-8'),
+            'score': detections['detection_scores'][i],
+        }
+        cleaned.append(d)
+        
+    return cleaned
