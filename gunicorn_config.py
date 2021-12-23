6ab6ff2241204a22b9bd66ccf3c5414e1119426c
Chris Chase <cchase@redhat.com>
2021-12-23 15:18:50 -0500

    Cleanup and reduce resources.

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 import os
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
-threads = int(os.environ.get('GUNICORN_THREADS', '1'))
+workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
+threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
 
