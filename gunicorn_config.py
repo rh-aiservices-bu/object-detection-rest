def179ef59f94d89af2176bc977e26b5100733ba
Christopher Chase <cchase@redhat.com>
2022-02-07 13:54:25 -0500

    Reduced number of gunicorn threads

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 import os
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
-threads = int(os.environ.get('GUNICORN_THREADS', '4'))
+threads = int(os.environ.get('GUNICORN_THREADS', '2'))
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
 
